9502dd45201fd966e09811f1419a69c59f6363de
nguyendangthao <nguyendangthaoit@gmail.com>
2021-09-02 00:35:25 +0800

    wheellucky ui

--- web_ui/src/app/user/components/LandingPage/LandingPage.js
+++ web_ui/src/app/user/components/LandingPage/LandingPage.js
@@ -4,15 +4,16 @@ import ListWinner from './ListWinner/ListWinner'
 import HowItWork from './HowItWork/HowItWork'
 import Questions from './Questions/Questions'
 import News from './News/News';
-
+import WheelLucky from './WheelLucky/main';
 function LandingPage(props) {
   return (
     <div className="landing-page">
-      <CountingDay/>
+      {/* <WheelLucky /> */}
+      <CountingDay />
       <ListWinner />
       <HowItWork />
       <Questions />
-      <News /> 
+      <News />
     </div>
   );
 }
