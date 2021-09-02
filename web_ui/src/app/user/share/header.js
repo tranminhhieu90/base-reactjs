61f5f965d039e85fd83859aefcde76f24804703f
hieu.tran <hieuminhtm90@gmail.com>
2021-09-02 13:00:08 +0700

    add menu in mobile

--- web_ui/src/app/user/share/header.js
+++ web_ui/src/app/user/share/header.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "app/user/assets/images/logo.png";
 const Header = (props) => {
+  const [showMenuMobile, setShowMenuMobile] = useState(false);
   return (
     <div className="header">
       <div className="header-logo">
@@ -14,6 +15,28 @@ const Header = (props) => {
           <li>FAQ</li>
           <li>News</li>
         </ul>
+      </div>
+      <div className="header-menu-icon">
+        <div
+          onClick={() => {
+            setShowMenuMobile(!showMenuMobile);
+          }}
+        >
+          <div className={`bar1 ${showMenuMobile ? "active" : ""}`}></div>
+          <div className={`bar2 ${showMenuMobile ? "active" : ""}`}></div>
+          <div className={`bar3 ${showMenuMobile ? "active" : ""}`}></div>
+        </div>
+        {showMenuMobile && (
+          <div className="menu-mobile">
+            <ul>
+              <li>Home</li>
+              <li>Lucky Wheel</li>
+              <li>Tutorial</li>
+              <li>FAQ</li>
+              <li>News</li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
