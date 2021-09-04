53fc7b25befb7dc5cc797c9b8c954f4098688c94
hieu.tran <hieuminhtm90@gmail.com>
2021-09-04 16:19:53 +0700

    fix ui final

--- web_ui/src/app/user/share/subFooter.js
+++ web_ui/src/app/user/share/subFooter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "app/user/assets/images/logo.png";
 const SubFooter = (props) => {
+  const url = "https://nerman.com.vn/";
   return (
     <div className="sub-footer">
       <div className="sub-footer-logo">
@@ -8,10 +9,34 @@ const SubFooter = (props) => {
       </div>
       <div className="sub-footer-menu">
         <ul>
-          <li>Trang Chủ</li>
-          <li>Vòng Quay</li>
+          <li
+            onClick={() => {
+              window.open(url, "_blank").focus();
+            }}
+          >
+            Trang Chủ
+          </li>
+          <li
+            onClick={() => {
+              const spinWheelElement = document.getElementById("spin-wheel");
+              if (spinWheelElement) {
+                spinWheelElement.scrollIntoView();
+              }
+            }}
+          >
+            Vòng Quay
+          </li>
           <li>Cửa Hàng</li>
-          <li>FAQ</li>
+          <li
+            onClick={() => {
+              const questionsElement = document.getElementById("questions");
+              if (questionsElement) {
+                questionsElement.scrollIntoView();
+              }
+            }}
+          >
+            FAQ
+          </li>
         </ul>
       </div>
     </div>
